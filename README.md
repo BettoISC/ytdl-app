98d019d0370d2b5ce2794f272854951d7d8b92d3
Beto Martinez <bettomb@gmail.com>
2021-05-25 04:35:57 +0000

    cleaning repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# ytdl-app
-This Repo is for a web app for download youtube videos
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-app="bettoisc/ytdl-app:latest"
-echo "Listening port=> $PORT"
-docker stop ytdl-app
-docker run  -d --rm --name ytdl-app -v $(pwd):/app ${app} -p $PORT -e LISTEN_PORT=$PORT
