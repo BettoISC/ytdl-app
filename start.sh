88d6ca09dd18feb8bc32bfaba86340e8371e33b4
Beto Martinez <bettomb@gmail.com>
2021-05-24 22:55:33 -0500

    update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 app="bettoisc/ytdl-app:latest"
 echo "Listening port=> $PORT"
 docker stop ytdl-app
-docker run  -d --rm --name ytdl-app -v $(pwd):/app ${app} -p $PORT -e LISTEN_PORT=$PORT
+docker run  -d --rm --name ytdl-app -v $(pwd):/app ${app} -p ${PORT} -e LISTEN_PORT=${PORT}
